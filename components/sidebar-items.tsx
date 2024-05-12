a7a7c65005678f7b8c5af11be8338b27e5e38eb9
Airenas Vaiciunas <airenass@gmail.com>
2024-05-12 11:53:09 +0300

    feat: drop uneeded features

--- components/sidebar-items.tsx
+++ components/sidebar-items.tsx
@@ -3,8 +3,6 @@
 import { Chat } from '@/lib/types'
 import { AnimatePresence, motion } from 'framer-motion'
 
-import { removeChat, shareChat } from '@/app/actions'
-
 import { SidebarActions } from '@/components/sidebar-actions'
 import { SidebarItem } from '@/components/sidebar-item'
 
@@ -27,13 +25,6 @@ export function SidebarItems({ chats }: SidebarItemsProps) {
                 height: 0
               }}
             >
-              <SidebarItem index={index} chat={chat}>
-                <SidebarActions
-                  chat={chat}
-                  removeChat={removeChat}
-                  shareChat={shareChat}
-                />
-              </SidebarItem>
             </motion.div>
           )
       )}
