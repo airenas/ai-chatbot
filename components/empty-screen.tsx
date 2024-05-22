c96610a48cfe54a2458bc0045c99c79aaef2095d
Airenas Vaiciunas <airenass@gmail.com>
2024-05-22 17:06:24 +0300

    feat: add initial greeting

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -1,20 +1,17 @@
-import { ExternalLink } from '@/components/external-link'
 
 export function EmptyScreen() {
   return (
     <div className="mx-auto max-w-2xl px-4">
       <div className="flex flex-col gap-2 rounded-lg border bg-background p-8">
         <h1 className="text-lg font-semibold">
-          Jus sveikina policijos robotas!
+          Laba diena!
         </h1>
         <p className="leading-normal text-muted-foreground">
-          Olia, olia, kokios pagalbos reikia šiandien?{' '} 
+          Aš Policijos departamento Informacijos teikimo skyriaus informavimo robotas <span
+            style={{ fontWeight: 'bold' }}> DiPOLIS</span>.
         </p>
         <p className="leading-normal text-muted-foreground">
-          Norite paklausyti {' '}
-          <ExternalLink href="https://www.youtube.com/watch?v=EBsgTJQFl9k?autoplay=1">
-          Rim Tim Tagi Dim
-          </ExternalLink>?
+          Kuo galiu padėti?
         </p>
       </div>
     </div>
