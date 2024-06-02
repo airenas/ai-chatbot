39b77d340678558f1f8a27600b3f64e5d4cf8a4c
Airenas Vaiciunas <airenass@gmail.com>
2024-06-02 11:34:32 +0300

    feat: display flags

--- components/lang-display.tsx
+++ components/lang-display.tsx
@@ -2,14 +2,27 @@
 
 
 import { useAppContext } from '@/app/app-context'
+import Flag from 'react-world-flags';
 
 export function LanguageDisplay() {
   const { language } = useAppContext()
 
+  function getFlagCode(lang: string): string {
+    const ls = lang.toUpperCase()
+    switch (ls) {
+      case 'EN':
+        return 'GB'
+      case 'UNK':
+        return 'LT'
+      default:
+        return ls
+    }
+  }
+
   return (
     <>
       {language !== 'lt' ? (
-        <><span color='red'>{language.toUpperCase()}</span><span> </span></>
+        <><Flag code={getFlagCode(language)} style={{ width: '25px' }} /></>
         ) : (
         <span></span>
       )}
