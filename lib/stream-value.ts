1a35408b1158f3bbbe9f71a6d2d664f42ccc7464
Airenas Vaiciunas <airenass@gmail.com>
2024-06-03 09:37:18 +0300

    fix: faster result output

--- lib/stream-value.ts
+++ lib/stream-value.ts
@@ -6,9 +6,25 @@ export class Streamer {
     }
 
     async *stringToAsyncIterable(): AsyncIterable<string> {
-        for (const char of this.data) {
-            yield char;
-            await new Promise(resolve => setTimeout(resolve, 5));
+        const minDelay = 5;
+        let delay = 5;
+        const l = this.data.length
+        for (let i = 0; i < this.data.length; i++) {
+            yield this.data[i];
+            if (i < 200) {
+                await new Promise(resolve => setTimeout(resolve, delay));
+            } else if (l - i < 200) {
+                delay = Math.min(minDelay, delay + 0.1);
+                await new Promise(resolve => setTimeout(resolve, delay));
+            }
+            else {
+                if (delay > 0.1) {
+                    delay = Math.max(0.1, delay - 0.1);
+                }
+                if (delay > 0.1 || i % 5 === 0) {
+                    await new Promise(resolve => setTimeout(resolve, delay));
+                }
+            }
         }
     }
 }
